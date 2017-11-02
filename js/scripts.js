dbf2e83ca7ea5ad7bb5a1c91bfd70f3c0737ebf8
eshull <31666670+eshull@users.noreply.github.com>
2017-11-01 22:19:05 -0700

    Initial commit

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+$(function() {
+	
+	// jQuery code goes here
+	
+	
+	 $('.flexslider').flexslider({
+         
+         animation: "slide",
+         animationSpeed: 2000,
+         pauseOnHover: "true"
+     });
+});
+	
